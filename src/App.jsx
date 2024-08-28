d13d19cb96b654a9c88f5e6c42fe4cd8bba3cff7
Nbin-lm <cosmoc834@gmail.com>
2024-08-28 13:28:59 +0545

    Mapping

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,39 @@
-import Parent from "./Stateprops/parent"
+import data from './data'
 function App() {
+
+  
+  const tdatacomplex = data.map((item) => (
+    <div key={item.Roll} style={{display:'flex',
+      border:"2px solid black",
+       color:"white",
+      justifyContent:'center',
+      gap:"40px",
+      padding:"30px 60px 30px 60px",
+      margin:"20px",
+      borderRadius:"20px",
+      boxShadow:"10px",
+    flexDirection:"column",
+      backgroundImage:"url('../image/Untitled.jpg')",
+      backgroundRepeat :'no-repeat',
+      backgroundSize :'cover',
+      width: 'fit-content'
+      
+      
+
+    }}> 
+      <div>Name={item.Name},<br/></div>
+      <div>Roll={item.Roll},<br/></div>
+      <div>Address={item.Address}</div> 
+    </div>
+  ));
+
   return (
     <div>
-      <Parent/>
+      <div style={{display:'grid',
+        gridTemplateColumns:'repeat(4,1fr)'
+      }}> {tdatacomplex}</div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/data.js
@@ -0,0 +1,54 @@
+const data = [
+  {
+    Name: "Asim",
+    Roll: 1,
+    Address: "Bhaktapur",
+  },
+  {
+    Name: "Sajal",
+    Roll: 2,
+    Address:"dhalmale",
+  },
+  {
+    Name:"Sushil",
+    Roll:3,
+    Address:"Ganesthan",
+  },
+  {
+    Name: "Samir",
+    Roll :4,
+    Address:"Dhanesor",
+  },
+  {
+    Name: "Sandesh",
+    Roll :5,
+    Address:"28kilo",
+  },
+  {
+    Name: "Bikal",
+    Roll :6,
+    Address:"Panauti",
+  },
+  {
+    Name: "Abishek",
+    Roll :7,
+    Address:"kushadevi",
+  },
+  {
+    Name: "Nabin",
+    Roll :8,
+    Address:"Bethanchowk",
+  },
+  {
+    Name: "Sanjeev",
+    Roll :9,
+    Address:"kathmandu",
+  },
+  {
+    Name: "Milan",
+    Roll :10,
+    Address:"Thadobato",
+  },
+  
+];
+export default data;
